a241994cfc8ae369f258e871fb4165c643c9b18f
Ghosty2004 <72819231+Ghosty2004@users.noreply.github.com>
2021-12-17 16:28:37 +0200

    Added removebuilding steal ;

--- mapstealer.lua
+++ mapstealer.lua
@@ -5,9 +5,15 @@ local ev = require('samp.events');
 
 --[[ Variables ]]
 local mapstealer = false;
+
 local objects = {};
 local count = 0;
 local temp_stream_data = {};
+
+local remove_objects = {};
+local remove_objects_count = 0;
+local temp_remove_building_data = {};
+local temp_remove_building_count = 0;
 
 --[[ Main ]]
 function main()
@@ -19,37 +25,56 @@ function main()
         if(mapstealer) then  
             SCM("Started.");
         else 
-            local map = "new ghosty2004_map;";
+            local map = "";
 
             local objects_count = 0;
             local materials_count = 0;
             local materialstext_count = 0;
+            local remove_building_count = 0;
 
             local maps = {};
 
+            table.insert(maps, "public OnFilterScriptInit()");
+            table.insert(maps, "{");
+
+            table.insert(maps, "    new ghosty2004_map;");
             for key, value in pairs(objects) do 
                 --[[ Objects ]]
                 if(value[1]) then
                     for key_object, value_object in pairs(value[1]) do 
                         objects_count = objects_count + 1; 
-                        table.insert(maps, string.format("%s", value_object));
+                        table.insert(maps, string.format("    %s", value_object));
                     end
                 end
                 --[[ Materials ]]
                 if(value[2]) then
                     for key_material, value_material in pairs(value[2]) do 
                         materials_count = materials_count + 1; 
-                        table.insert(maps, string.format("%s", value_material));
+                        table.insert(maps, string.format("    %s", value_material));
                     end
                 end
                 --[[ Materials Text ]]
                 if(value[3]) then 
                     for key_materialtext, value_materialtext in pairs(value[3]) do 
                         materialstext_count = materialstext_count + 1; 
-                        table.insert(maps, string.format("%s", value_materialtext));
+                        table.insert(maps, string.format("    %s", value_materialtext));
                     end;
                 end
             end
+
+            table.insert(maps, "}");
+
+            table.insert(maps, "\n");
+
+            table.insert(maps, "public OnPlayerConnect(playerid)");
+            table.insert(maps, "{");
+
+            for key, value in pairs(remove_objects) do 
+                table.insert(maps, string.format("    %s", value));
+                remove_building_count = remove_building_count + 1;
+            end
+
+            table.insert(maps, "}");
 
             for key, value in pairs(maps) do map = string.format("%s\n%s", map, value); end 
 
@@ -57,7 +82,7 @@ function main()
                 if(value.label) then sampDestroy3dText(value.label); end 
             end 
 
-            SCM(string.format("Saved: %d objects, %d materials and %d materials text.", objects_count, materials_count, materialstext_count));
+            SCM(string.format("Saved: %d objects, %d remove buildings, %d materials and %d materials text.", objects_count, remove_building_count, materials_count, materialstext_count));
             
             local header = "/* ======================== */\n/* Ghosty2004's map stealer */\n/* ======================== */";
 
@@ -76,9 +101,22 @@ function main()
     while true do 
         wait(0);
         if(mapstealer) then 
+            --[[ Remove Building ]]
+            local pPositionX, pPositionY, pPositionZ = getCharCoordinates(PLAYER_PED);
+            for key, value in pairs(temp_remove_building_data) do
+                local distance = getDistanceBetweenCoords3d(pPositionX, pPositionY, pPositionZ, value.position.x, value.position.y, value.position.z)
+                if(distance <= 400) then 
+                    remove_object_string = string.format("RemoveBuildingForPlayer(playerid, %d, %f, %f, %f, %f);", value.modelId, value.position.x, value.position.y, value.position.z, value.radius)
+                    if(not checkIfThisRemoveBuildingSrcExists(remove_object_string)) then 
+                        table.insert(remove_objects, remove_object_string);
+                    end
+                end
+            end
+
             local objects_count = 0;
             local materials_count = 0;
             local materialstext_count = 0;
+            local remove_building_count = 0;
 
             for key, value in pairs(objects) do 
                 if(value[1]) then for _ in pairs(value[1]) do objects_count = objects_count + 1; end  end 
@@ -86,7 +124,9 @@ function main()
                 if(value[3]) then for _ in pairs(value[3]) do materialstext_count = materialstext_count + 1; end end
             end
 
-            info(string.format("Recording...~w~~n~objects: ~y~~h~%d~w~, materials: ~y~~h~%d~w~, materials text: ~y~~h~%d", objects_count, materials_count, materialstext_count), 1);
+            for key, value in pairs(remove_objects) do remove_building_count = remove_building_count + 1; end;
+
+            info(string.format("Recording...~n~~w~~h~Objects: ~y~~h~%d ~w~~h~| Remove Buildings: ~y~~h~%d ~n~~w~~h~Materials: (~y~~h~%d~w~ Textures | ~y~~h~%d~w~~h~ Texts)", objects_count, remove_building_count, materials_count, materialstext_count), 1);
         end 
     end 
 end
@@ -98,6 +138,15 @@ function onExitScript(quitGame)
 end 
 
 --[[ Events ]]
+function ev.onRemoveBuilding(modelId, position, radius) 
+    temp_remove_building_count = temp_remove_building_count + 1;
+    temp_remove_building_data[temp_remove_building_count] = {
+        modelId = modelId;
+        position = position;
+        radius = radius;
+    }
+end 
+
 function ev.onDestroyObject(objectId)
     if(temp_stream_data[objectId]) then sampDestroy3dText(temp_stream_data[objectId].label); end
     temp_stream_data[objectId] = {};
@@ -111,7 +160,7 @@ function ev.onCreateObject(objectId, data)
             if(not objects[count]) then
                 objects[count] = {};
                 objects[count][1] = {};
-                table.insert(objects[count][1], string.format("%s", object_string));
+                table.insert(objects[count][1], object_string);
                 local positionX, positionY, positionZ = getCharCoordinates(PLAYER_PED);
                 addOneOffSound(positionX, positionY, positionZ, 1058);
             end
@@ -135,7 +184,7 @@ function ev.onSetObjectMaterial(objectId, data)
                 local material_string = string.format("SetDynamicObjectMaterial(ghosty2004_map, %d, %d, \"%s\", \"%s\", %d);", data.materialId, data.modelId, data.libraryName, data.textureName, data.color);
                 if(not isMaterialExists(index, material_string)) then 
                     if(not objects[index][2]) then objects[index][2] = {}; end
-                    table.insert(objects[index][2], string.format("%s", material_string));
+                    table.insert(objects[index][2], material_string);
                     updateThisLabel(index);
                 end
             end
@@ -151,7 +200,7 @@ function ev.onSetObjectMaterialText(objectId, data)
                 local materialtext_string = string.format("SetDynamicObjectMaterialText(ghosty2004_map, %d, \"%s\", %d, \"%s\", %d, %d, %d, %d, %d);", data.materialId, data.text, data.materialSize, data.fontName, data.fontSize, data.bold, data.fontColor, data.backGroundColor, data.align);
                 if(not isMaterialTextExists(index, materialtext_string)) then
                     if(not objects[index][3]) then objects[index][3] = {}; end
-                    table.insert(objects[index][3], string.format("%s", materialtext_string));
+                    table.insert(objects[index][3], materialtext_string);
                     updateThisLabel(index);
                 end
             end
@@ -175,6 +224,14 @@ function updateThisLabel(labelIndex)
             sampSet3dTextString(value.label, string.format("Object Info:\nID: %s | Model: %s\nMaterials: %s | Materials Text: %s", value.index, value.objectInfo.model, getMaterialCount(value.index), getMaterialTextCount(value.index)));
         end 
     end
+end 
+
+function checkIfThisRemoveBuildingSrcExists(src) 
+    local exists = false;
+    for key, value in pairs(remove_objects) do 
+        if(value == src) then exists = true; end
+    end 
+    return exists;
 end 
 
 function checkIfThisObjectSrcExists(src) 
